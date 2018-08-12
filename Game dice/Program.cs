2642fd33868ce567b56e16d95bef404abce54e9d
wambui7 <shiko.sheila@gmail.com>
2018-08-12 17:35:48 -0500

    Add project files.

---
+++ Game dice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
