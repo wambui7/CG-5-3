9480a150cd0a898497e95efce9ab8cd5a27331b1
wambui7 <shiko.sheila@gmail.com>
2018-08-12 18:15:21 -0500

    no changes

--- Game dice/Program.cs
+++ Game dice/Program.cs
@@ -6,7 +6,54 @@ namespace Game_dice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+
+            int computerplayer = 0;
+
+            int playerone = 0;
+
+
+            Console.WriteLine("play a game");
+
+            Console.WriteLine("if you  Get To 20 First, you win");
+
+            Console.Write("Please enter a name and hit enter: ");
+
+            var playerName = Console.ReadLine();
+
+            do
+            {
+                computerplayer += RollDice();
+
+                Console.WriteLine("Player: " + computerplayer);
+
+                computerplayer += RollDice();
+                Console.WriteLine("Player: " + playerone);
+
+            } while (playerone <= 20 && computerplayer <= 20);
+            if (computerplayer >= 20)
+            {
+                Console.WriteLine("you win");
+            }
+            else if (playerone >= 20)
+            {
+                Console.WriteLine("you win");
+            }
+            else
+            {
+
+                Console.WriteLine("BLANK");
+            }
+            Console.ReadLine();
+        }
+
+        private static int RollDice()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(1, 6);
         }
     }
 }
+    
+
